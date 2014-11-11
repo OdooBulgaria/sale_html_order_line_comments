7691f7797b5a118a267a8f027c3717be9ced36d2
lukebranch <lukebranch@gmail.com>
2014-11-11 16:32:50 +0800

    Create __openerp__.py

---
+++ __openerp__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Quotation & Sale Order Line Comments',
+    'description': 'Adds a HTML field to quotation/saleorder and invoice reports to allow for HTML styling and pictures on order lines.',
+    'category': 'Website',
+    'version': '1.0',
+    'author': 'Luke Branch',
+    'depends': ['sale'],
+    'data': [
+        'views/layout.xml',
+    ],
+    'application': True,
+}
